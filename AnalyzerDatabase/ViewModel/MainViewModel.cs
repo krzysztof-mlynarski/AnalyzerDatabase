ad5787444e6b0f730d6a3fdb309a7d9455530146
Krzysztof Młynarski <kmlynarski@wi.zut.edu.pl>
2016-04-22 14:11:08 +0200

    Added pattern MVVM Light

---
+++ AnalyzerDatabase/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace AnalyzerDatabase.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
