e02f218fc2c2df7225b36ae61fbb47b2fe44a6df
Krzysztof Młynarski <krzysztof-mlynarski@o2.pl>
2016-08-25 12:05:42 +0200

    Added Services for interfaces

---
+++ AnalyzerDatabase/Services/DeserializeJsonService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AnalyzerDatabase.Interfaces;
+using Newtonsoft.Json;
+
+namespace AnalyzerDatabase.Services
+{
+    public class DeserializeJsonService : IDeserializeJsonService
+    {
+        public T GetObjectFromJson<T>(string jsonToDeserialize)
+        {
+            if (!String.IsNullOrEmpty(jsonToDeserialize))
+            {
+                try
+                {
+                    T tagObject = JsonConvert.DeserializeObject<T>(jsonToDeserialize);
+                    return tagObject;
+                }
+                catch (Exception)
+                {
+                    return default(T);
+                }
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ AnalyzerDatabase/Services/InternetConnectionService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using AnalyzerDatabase.Interfaces;
+
+namespace AnalyzerDatabase.Services
+{
+    public class InternetConnectionService : IInternetConnectionService
+    {
+        //public bool CheckConnection()
+        //{
+        //    ConnectionProfile internetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
+
+        //    if (internetConnectionProfile == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    return true;
+        //}
+
+        public async Task<bool> IsNetworkAvailable()
+        {
+            if (!(await Task.Run(() => NetworkInterface.GetIsNetworkAvailable())))
+                return false;
+            else
+                return true;
+        }
+
+        //public bool IsInternetAccess()
+        //{
+        //    if (!(NetworkInformation.GetInternetConnectionProfile().GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess))
+        //        return false;
+        //    else
+        //        return true;
+        //}
+    }
+}
