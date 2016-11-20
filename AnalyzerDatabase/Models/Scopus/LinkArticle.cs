cb379446cc28a7cb2375c438927b1c9dcf6e657a
Krzysztof Młynarski <krzysztof-mlynarski@o2.pl>
2016-11-20 20:00:58 +0100

    Added models for database Scopus

---
+++ AnalyzerDatabase/Models/Scopus/LinkArticle.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace AnalyzerDatabase.Models.Scopus
+{
+    public class LinkArticle
+    {
+        [JsonProperty("@_fa")]
+        public string Fa { get; set; }
+
+        [JsonProperty("@ref")]
+        public string Ref { get; set; }
+
+        [JsonProperty("@href")]
+        public string Href { get; set; }
+
+        public LinkArticle(string fa, string @ref, string href)
+        {
+            Fa = fa;
+            Ref = @ref;
+            Href = href;
+        }
+    }
+}
---
+++ AnalyzerDatabase/Models/Scopus/ScopusSearchQuery.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace AnalyzerDatabase.Models.Scopus
+{
+    public class ScopusSearchQuery
+    {
+        [JsonProperty("search-results")]
+        public SearchResults SearchResults { get; set; }
+
+        public ScopusSearchQuery(SearchResults searchResults)
+        {
+            SearchResults = searchResults;
+        }
+    }
+}
