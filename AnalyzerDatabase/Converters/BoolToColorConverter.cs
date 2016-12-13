42bb3343275c999a9ead354ba1904e32ab8f9b7f
Krzysztof Młynarski <krzysztof-mlynarski@o2.pl>
2016-12-13 22:13:09 +0100

    Added method for color conversion in DataGridzie

---
+++ AnalyzerDatabase/Converters/BoolToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AnalyzerDatabase.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = (bool)value;
+
+            if (val)
+            {
+                return Brushes.Red;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
