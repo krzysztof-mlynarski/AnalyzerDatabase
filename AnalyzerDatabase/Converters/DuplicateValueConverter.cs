5d9f45b9d1284cba7d184362c565b3197c2e978b
Krzysztof Młynarski <krzysztof-mlynarski@o2.pl>
2016-12-15 15:28:54 +0100

    Added new converters to properly recognize duplicates

---
+++ AnalyzerDatabase/Converters/DuplicateValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AnalyzerDatabase.Converters
+{
+    public class DuplicateValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().StartsWith(parameter.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AnalyzerDatabase/Converters/GroupsToTotalConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace AnalyzerDatabase.Converters
+{
+    public class GroupsToTotalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is ReadOnlyObservableCollection<Object>)
+            {
+                var items = (ReadOnlyObservableCollection<Object>)value;
+                Decimal total = 0;
+                foreach (GroupItem gi in items)
+                {
+                    total += gi.TabIndex;
+                }
+                return total.ToString();
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
